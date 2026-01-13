de8805b99b409691059e512e201483b41aebbfc8
trzynasty-0 <goniwiechamaja123@gmail.com>
2026-01-13 08:22:35 +0100

    wyświetla liste

--- app/src/main/java/com/example/listypowtorka/MainActivity.java
+++ app/src/main/java/com/example/listypowtorka/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.listypowtorka;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private Button dodajB;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> rzeczyDoZrobienia;
+    private EditText opisNotatki;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        dodajB = findViewById(R.id.dodajB);
+        listView = findViewById(R.id.lista);
+        opisNotatki = findViewById(R.id.elementET);
+
+        rzeczyDoZrobienia = new ArrayList<>();
+        rzeczyDoZrobienia.add("Wyjście  do kina");
+        rzeczyDoZrobienia.add("nauczyć sie");
+        rzeczyDoZrobienia.add("fdsjkfrz");
+
+        arrayAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                rzeczyDoZrobienia);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
